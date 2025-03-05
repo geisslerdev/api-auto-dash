d3c293b6b028d1202f1d89420f41af047e336a79
geisslerdev <davi@anota.ai>
2025-03-05 03:50:00 -0300

    conexao com db

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,11 @@ import { IfoodController } from './controllers/ifood.controller';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || '192.168.0.37',
+      host: process.env.DB_HOST || 'database-1.cf64eqcyyvd7.us-east-2.rds.amazonaws.com',
       port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'senhapostgres',
-      database: process.env.DB_NAME || 'postgres',
+      username: process.env.DB_USER || 'dbadmin',
+      password: process.env.DB_PASS || 'dbPassword1108',
+      database: process.env.DB_NAME || 'mydatabase',
       entities: [User, Merchant, Subscription],
       synchronize: true,
     }),
